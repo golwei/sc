6ad69e69d2e772112b39f4f47071bfc762444076
golwei <lizhiweimail@qq.com>
2018-08-18 12:10:26 +0800

    test commit

--- dist/pages/cart.js
+++ dist/pages/cart.js
@@ -1,8 +1,21 @@
-'use strict';
+"use strict";
 
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+// fixedView2.js
 exports.default = Page({
-  data: {}
+  data: {
+    NAV_HEIGHT: wx.STATUS_BAR_HEIGHT + wx.DEFAULT_HEADER_HEIGHT + "px",
+    arry: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    isShow: true
+  },
+  show: function show() {
+    this.setData({
+      isShow: false
+    });
+  },
+  navigateBack: function navigateBack() {
+    wx.navigateBack();
+  }
 });
--- dist/pages/me.js
+++ dist/pages/me.js
@@ -1,8 +1,29 @@
-'use strict';
+"use strict";
 
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.default = Page({
-  data: {}
+  data: {
+    NAV_HEIGHT: wx.STATUS_BAR_HEIGHT + wx.DEFAULT_HEADER_HEIGHT + "px",
+    STATUS_BAR_HEIGHT: wx.STATUS_BAR_HEIGHT,
+    url: "http://images.uileader.com/20180321/9e5ff8b2-d6a1-4a56-ab63-88bcd00560b0.png",
+    items: [],
+    alpha: 0,
+    className: ""
+  },
+  navigateBack: function navigateBack() {
+    wx.navigateBack();
+  },
+
+  onReady: function onReady() {
+    var arr = [];
+    for (var i = 1; i <= 5; i++) {
+      arr.push("\u5217\u8868\u9879\u76EE" + i);
+    }
+    this.setData({
+      items: arr
+    });
+    console.log(this.data.items);
+  }
 });
